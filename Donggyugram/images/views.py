669cc3d23235448ec6ce91dd134c78a3bb541fd1
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-05-01 22:27:22 +0900

    unliking an image

--- Donggyugram/images/views.py
+++ Donggyugram/images/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import models, serializers
+from rest_framework import status
+
+
 
 # Create your views here.
 
@@ -45,14 +48,25 @@ class LikeImage(APIView):
         try:
           found_image = models.Image.objects.get(id = image_id)
         except models.Image.DoesNotExist:
-            return Response(status=404)
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
+        try:
+            preexisting_like = models.Like.objects.get(
+                creator = user,
+                image = found_image,
+            )
+            preexisting_like.delete()
 
-        new_like = models.Like.objects.create(
-            creator = user,
-            image = found_image
-        )
+            
 
-        new_like.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except models.Like.DoesNotExist:
 
-        return Response(status = 200)
+            new_like = models.Like.objects.create(
+                creator = user,
+                image = found_image
+            )
+
+            new_like.save()
+
+            return Response(status = status.HTTP_201_CREATED)
