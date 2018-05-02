45af9fc343585f5fae23c2381071be5447671cac
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-05-02 15:02:57 +0900

    seperating like/unlike

--- Donggyugram/images/urls.py
+++ Donggyugram/images/urls.py
@@ -7,9 +7,14 @@ app_name = "images"
 urlpatterns = [
     url(regex=r"^$", view=views.Feed.as_view(), name="feed"),
     url(
-        regex=r"^(?P<image_id>[0-9]+)/likes/$",
+        regex=r"^(?P<image_id>[0-9]+)/like/$",
         view= views.LikeImage.as_view(),
         name="like_image",
+    ),
+    url(
+        regex=r'(?P<image_id>[0-9]+)/unlike/$',
+        view= views.UnLikeImage.as_view(),
+        name='unlike_image'
     ),
     url(
         regex=r"^(?P<image_id>[0-9]+)/comments/$",
