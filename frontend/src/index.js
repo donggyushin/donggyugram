c1f59396a440555dea94fb63cbd6cddf5010910f
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-05-19 00:28:03 +0900

    Redux Middlewares

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import store from "./redux/configureStore";
+import store from "redux/configureStore";
 
 console.log(store.getState());
 
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,10 +1,21 @@
-import { combineReducers, createStore } from "redux";
-import users from "./moduels/users"; //이 부분 살짝 의심감. reducer를 import해야하는것 아닌가?
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import users from "redux/moduels/users"; //이 부분 살짝 의심감. reducer를 import해야하는것 아닌가?
+import thunk from "redux-thunk";
+
+const env = process.env.NODE_ENV;
+
+const middlewares = [thunk];
+
+if (env === "development") {
+  const logger = require("redux-logger");
+  middlewares.push(logger);
+}
 
 const reducer = combineReducers({
   users
 });
 
-let store = initialState => createStore(reducer);
+let store = initialState =>
+  createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();
