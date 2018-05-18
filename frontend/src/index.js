0cf8d59c1f330f2775d3810dae791bdb265ae0e4
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-05-18 15:38:16 +0900

    connectiong the Reducer with the react App

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import store from "./redux/configureStore";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log(store.getState());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,5 +1,5 @@
 import { combineReducers, createStore } from "redux";
-import users from "./moduels";
+import users from "./moduels/users"; //이 부분 살짝 의심감. reducer를 import해야하는것 아닌가?
 
 const reducer = combineReducers({
   users
