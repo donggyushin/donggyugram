92bc56119edb038f8dcbaa8c1abd222c32d12f57
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-05-22 19:45:27 +0900

    Auth Component part one

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,15 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import Footer from "components/Footer";
-import styles from "./styles.scss";
+import "./styles.scss";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import PropTypes from "prop-types";
+import Auth from "../Auth/presenter";
 
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.App}>
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = props => [
+  //nav,
+  props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
+  <Footer key={3} />
+];
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
+};
+
+const PrivateRoutes = props => (
+  <Switch>
+    <Route exact path="/" render={() => "feed"} />
+    <Route exact path="/explore" render={() => "explore"} />
+  </Switch>
+);
+
+const PublicRoutes = props => (
+  <Switch>
+    <Route exact path="/" component={Auth} />
+    <Route exact path="/forgot" render={() => "password"} />
+  </Switch>
+);
 
 export default App;
---
+++ frontend/src/components/Auth/index.js
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import Container from "./container";
+
+//Add all the actions for..
+//login
+//sign up
+//recover password
+//check username
+//check password
+//check email
+
+export default connect()(Container);
