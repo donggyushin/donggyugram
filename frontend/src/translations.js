c41c26be880d23bb92da789072cd79fca63054f9
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-05-25 16:03:31 +0900

    importing translations

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,10 +1,23 @@
 export const translations = {
-  es: {
-    "Translate this text": "Traduce este texto",
-    "Hello {n}!": "Hola {n}!"
+  'kr': {
+    'Don\'t have an account?': '계정이 없나요?',
+    'Sign up': '회원가입',
+    'Have an account?': '계정이 있나요?',
+    'Log in': '로그인',
+    'Get the app': '',
+    'with Facebook': '',
+    'Forgot password?': '비밀번호를 잊어버렸나요?',
+    'Sign up to see photos and videos from your friends.': '친구들의 사진과 비디오를 보려면 로그인하세요.',
+    'Log in with Facebook': '페이스북으로 로그인하기',
+    'or': '또는',
+    'Email': '',
+    'Full Name': '',
+    'Username': '',
+    'Password': '',
+    'By signing up, you agree to our': '',
+    'Terms & Privacy Policy': '',
+    'About Us': '',
   },
-  kr: {
-    "Log in": "로그인",
-    "About Us": "우리에 대해서"
-  }
-};
+  'options': {
+  },
+}
