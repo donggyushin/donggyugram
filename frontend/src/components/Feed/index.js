72dc2358b459b0c3e16da3d424c59380e4a0a812
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-05-31 23:24:09 +0900

    RenderFeed Component and Persistence

--- frontend/src/components/Feed/index.js
+++ frontend/src/components/Feed/index.js
@@ -2,8 +2,17 @@ import { connect } from "react-redux";
 import Container from "./container";
 import { actionCreators as photoActions } from "../../redux/moduels/photos";
 
+const mapStateToProps = state => {
+  const {
+    photos: { feed }
+  } = state;
+  return {
+    feed
+  };
+};
+
 const mapDispatchToProps = dispatch => ({
   getFeed: () => dispatch(photoActions.getFeed())
 });
 
-export default connect(null, mapDispatchToProps)(Container);
+export default connect(mapStateToProps, mapDispatchToProps)(Container);
--- frontend/src/redux/moduels/photos.js
+++ frontend/src/redux/moduels/photos.js
@@ -4,7 +4,16 @@ import { actionCreators as userActions } from "./user";
 
 //actions
 
+const SET_FEED = "SET_FEED";
+
 //action creators
+
+function setFeed(feed) {
+  return {
+    type: SET_FEED,
+    feed
+  };
+}
 
 //api actions
 
@@ -25,7 +34,7 @@ function getFeed() {
         }
         return response.json();
       })
-      .then(json => console.log(json));
+      .then(json => dispatch(setFeed(json)));
   };
 }
 
@@ -36,12 +45,22 @@ const initialState = {};
 
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case SET_FEED:
+      return applySetFeed(state, action);
     default:
       return state;
   }
 }
 
 //reducer function
+
+function applySetFeed(state, action) {
+  const { feed } = action;
+  return {
+    ...state,
+    feed
+  };
+}
 
 //export actions
 
