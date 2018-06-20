6cd5c7e41a73d027c425552f40fd151e26bfdf83
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-06-20 23:10:03 +0900

    Search Component pt. One

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,5 +1,12 @@
-import React from "react";
 import Container from "./container";
 import { connect } from "react-redux";
+import { push } from "react-router-redux";
 
-export default connect()(Container);
+const mapDispatchToProps = dispatch => ({
+  goToSearch: searchTerm => dispatch(push(`/search/${searchTerm}`))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Container);
